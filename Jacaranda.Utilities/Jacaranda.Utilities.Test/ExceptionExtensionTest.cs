819b1df1e33d898ecc5b2f1aa1a5728fcf4f7f9b
Ken Li <likan971@gmail.com>
2018-06-13 11:07:02 +0800

    Utilities Init

---
+++ Jacaranda.Utilities/Jacaranda.Utilities.Test/ExceptionExtensionTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jacaranda.Utilities.Test
+{
+    [TestClass]
+    public class ExceptionExtensionTest
+    {
+        [TestMethod]
+        public void GetFullMessagesTest()
+        {
+            var ex = new Exception("This is a test exception", new Exception("This is the inner exception"));
+            Assert.AreEqual(ex.GetFullMessages("-->"), "This is a test exception --> This is the inner exception");
+        }
+    }
+}
---
+++ Jacaranda.Utilities/Jacaranda.Utilities.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Jacaranda.Utilities.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Jacaranda.Utilities.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b6cebd85-d090-4639-bcea-5d8a6d66ce0b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
