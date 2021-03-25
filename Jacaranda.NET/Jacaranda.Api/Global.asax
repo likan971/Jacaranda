b2d8a308be923c00b2c68905056e9d463a77c1ca
likan971 <likan971@gmail.com>
2021-03-25 23:34:53 +0800

    Remove old project

--- Jacaranda.NET/Jacaranda.Api/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jacaranda.Api.WebApiApplication" Language="C#" %>
--- Jacaranda.NET/Jacaranda.Data/Interfaces/IErrorLoggerService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Jacaranda.Data
-{
-    public interface IErrorLoggerService
-    {
-        Task LogErrorToDbAsync(string requestUrl, string requestBody, string message, string stackTrace);
-    }
-}
